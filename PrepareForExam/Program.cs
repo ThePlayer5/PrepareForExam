ba723fb1a98b0a412d783a1049c8effe34c50878
212301 <212301@1220-010-22>
2025-06-17 16:37:22 +0300

    Добавьте файлы проекта.

---
+++ PrepareForExam/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrepareForExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            //Console.WriteLine("Enter 2 numbers: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 > num2)
+            //    Console.WriteLine($"Bigger is {num1}");
+            //else if (num1 < num2)
+            //    Console.WriteLine($"Bigger is {num2}");
+            //else Console.WriteLine("Equls");
+
+            // Task 2
+            //Console.WriteLine("Enter number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num % 2 == 0)
+            //    Console.WriteLine("Chetnoe");
+            //else Console.WriteLine("Ne chetnoe");
+
+            // Task 3
+            //Console.WriteLine("Enter 2 numbers: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter operation: ");
+            //char operation = Convert.ToChar(Console.ReadLine());
+            //if (operation == '+')
+            //    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            //else if (operation == '-')
+            //    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            //else if (operation == '*')
+            //    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            //else if (operation == '/')
+            //    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+
+            // Task 4
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.WriteLine();
+            //        Console.Write($"{i} * {j} = {i*j}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // Task 5
+            //Console.WriteLine("Enter number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int answer = 1;
+            //for (int i = num; i >= 1; i--)
+            //{
+            //    answer *= i;
+            //}
+            //Console.WriteLine(answer);
+
+            // Task 6
+            //Console.WriteLine("Enter number: ");
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //int fobonachchi = 0;
+            //int[] mass = new int[N];
+            //mass[0] = 1;
+            //mass[1] = 1;
+            //for(int i = 2; i < N; i++)
+            //{
+            //    mass[i] = mass[i - 2] + mass[i - 1];
+            //}
+            //foreach (int i in mass)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            // Task 7
+            //Console.WriteLine("Enter number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int c = 0;
+            //int k = num;
+            //int result = 0;
+            //while (k != 0)
+            //{
+            //    c = k % 10;
+            //    result = result + c;
+            //    k /= 10;
+            //}
+            //Console.WriteLine(result);
+
+            // Task 8
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine(); // aboba
+            string str2 = str;
+            string str3 = string.Empty;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i == str.Length - i - 1)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ PrepareForExam/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PrepareForExam")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrepareForExam")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("01c07ac1-87de-4044-87e6-07cdf0947820")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
